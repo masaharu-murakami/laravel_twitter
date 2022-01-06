c7af976a75442e88f600e59903fbc7750aa1ad24
masaharu-murakami <puraema8@gmail.com>
2022-01-06 07:32:46 +0000

    fix footer and add viewport setting

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,6 +2,7 @@
   <head>
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <link href="{{ asset('css/app.css') }}"rel="stylesheet">
   </head>
   <body>
